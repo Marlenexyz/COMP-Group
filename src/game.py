d1a7a32a40819dc4ebf3af879fe42334c916abfb
LeonardoSchoch <leonardoschoch@gmail.com>
2023-12-11 11:02:17 +0000

    change game to be able to work just one index finger detection

--- src/game.py
+++ src/game.py
@@ -29,41 +29,29 @@ if __name__ == '__main__':
         ret, frame = cap.read()
         
         game_corners = frame_matching.run(frame)
-        index_finger_pos = hand_recognition.run(frame)
+        hand_recognition.run(frame)
+        index_finger_pos = [hand_recognition.getIndexFingerCoordLeft, hand_recognition.getIndexFingerCoordRight]
         cv2.imshow('frame', frame)
         
-        if game_corners is not None:
-            # sort gamecorners by upper left, lower left, upper right, lower right
-            game_corners_sorted = sorted(game_corners, key=lambda corner: corner[0] ** 2 + corner[1] ** 2)
-        if index_finger_pos is not None:
-            # sort index finger pos by x value
-            index_finger_pos_sorted = sorted(index_finger_pos, key=lambda pos: pos[0])
-        
-        try:
+        # check if left index finger was detected
+        if index_finger_pos[0] is not None:
             # get left y values
-            left_y_min, left_y_max = game_corners_sorted[0][1], game_corners_sorted[1][1]
-            left_y_crt = index_finger_pos_sorted[0][1]
-            # get right y values
-            right_y_min, right_y_max = game_corners_sorted[2][1], game_corners_sorted[3][1]
-            right_y_crt = index_finger_pos_sorted[1][1]
-        
+            left_y_min, left_y_max = game_corners[0][1], game_corners[1][1]
+            left_y_crt = index_finger_pos[0][1]
             # check if left index finger is in game frame
             if left_y_crt >= left_y_min and left_y_crt < left_y_max:
                 left_y_new = (left_y_crt - left_y_min) / (left_y_max - left_y_min)
-            else:
-                left_y_new = None
+                pong.move_paddle_left(left_y_new * game_height)
+                
+        # check if right index finger was detected
+        if index_finger_pos[1] is not None:
+            # get right y values
+            right_y_min, right_y_max = game_corners[2][1], game_corners[3][1]
+            right_y_crt = index_finger_pos[1][1]
             # check if right index finger is in game frame
             if right_y_crt >= right_y_min and right_y_crt < right_y_max:
                 right_y_new = (right_y_crt - right_y_min) / (right_y_max - right_y_min)
-            else:
-                right_y_new = None
-            
-            if left_y_new is not None:
-                pong.move_paddle_left(left_y_new * game_height)
-            if right_y_new is not None:
                 pong.move_paddle_right(right_y_new * game_height)
-        except:
-            pass
 
         pong.run()
 
